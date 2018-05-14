e6c0bca2f382076adaa124b001803803a998769d
Mattias Cibien <mattias@mattiascibien.net>
2018-05-14 17:38:23 +0200

    feat(animarender): add file information on Windows
    added file information for the windows platform
    including copyright and product name

---
+++ animarender/build.rs
@@ -0,0 +1,23 @@
+#[cfg(target_os = "windows")]
+extern crate winres;
+
+#[cfg(target_os = "windows")]
+fn main() {
+    use std::io::Write;
+    // only build the resource for release builds
+    // as calling rc.exe might be slow
+    if std::env::var("PROFILE").unwrap() == "release" {
+        let res = winres::WindowsResource::new();
+        match res.compile() {
+            Err(e) => {
+                write!(std::io::stderr(), "{}", e).unwrap();
+                std::process::exit(1);
+            }
+            Ok(_) => {}
+        }
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {
+}
