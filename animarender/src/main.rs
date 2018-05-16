eb094ba02a57e20557648a8021fc32ac56585e49
Mattias Cibien <mattias@mattiascibien.net>
2018-05-16 11:15:08 +0200

    feat(animarender,libanima): add rendering window
    Added a window where rendering will be executed.
    At the moment it is OpenGL only but more backends are planned in the
    future

--- animarender/src/main.rs
+++ animarender/src/main.rs
@@ -1,5 +1,35 @@
 extern crate anima;
 
+#[macro_use]
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    
+    let matches = App::new("AnimaRender")
+        .version(crate_version!())
+        .author("Mattias Cibien <mattias@mattiascibien.net>")
+        .about("3D Rendering Engine")
+        .arg(Arg::with_name("vsync")
+                .help("Sets vsync enabled"))
+        .arg(Arg::with_name("width")
+                .help("The width of the rendering window")
+                .value_name("WIDTH")
+                .takes_value(true))
+        .arg(Arg::with_name("height")
+                .help("The height of the rendering window")
+                .value_name("HEIGHT")
+                .takes_value(true))
+        .arg(Arg::with_name("scene")
+                .help("The scene file to render")
+                .value_name("FILE")
+                .takes_value(true))
+        .get_matches();
+
+    let width = matches.value_of("width").unwrap_or("800").parse::<u32>().unwrap();
+    let height = matches.value_of("height").unwrap_or("600").parse::<u32>().unwrap();
+
+    let vsync =  matches.is_present("vsync");
+
+    anima::render(width, height, vsync, "");
 }
--- libanima/src/lib.rs
+++ libanima/src/lib.rs
@@ -1,3 +1,51 @@
+extern crate gfx;
+
+extern crate glutin;
+extern crate gfx_window_glutin;
+
+use gfx::Device;
+use gfx_window_glutin as gfx_glutin;
+use glutin::{GlContext, GlRequest};
+use glutin::Api::OpenGl;
+
+type ColorFormat = gfx::format::Srgba8;
+type DepthFormat = gfx::format::DepthStencil;
+
+const WINDOW_TITLE: &str = "Anima Render";
+
+const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+
+pub fn render(width: u32, height: u32, vsync: bool, file: &str) {
+    let mut events_loop = glutin::EventsLoop::new();
+    let builder = glutin::WindowBuilder::new()
+        .with_title(WINDOW_TITLE)
+        .with_dimensions(width, height);
+    let context = glutin::ContextBuilder::new()
+        .with_gl(GlRequest::Specific(OpenGl,(3,2)))
+        .with_vsync(vsync);
+
+    let (window, mut device, mut _factory, _rtv, mut _stv) = gfx_glutin::init::<ColorFormat, DepthFormat>(builder, context, &events_loop);
+
+    let mut running = true;
+    while running {
+        events_loop.poll_events(|event| {
+            if let glutin::Event::WindowEvent { event, .. } = event {
+                match event {
+                    glutin::WindowEvent::KeyboardInput {
+                        input: glutin::KeyboardInput {
+                            virtual_keycode: Some(glutin::VirtualKeyCode::Escape), ..
+                        }, ..
+                    } => running = false,
+                    _ => {}
+                }
+            }
+        });
+
+        window.swap_buffers().unwrap();
+        device.cleanup();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
